6037e68bea912114d27152111596413c6e48135e
Brian MANGOENPAWIRO (contractor) <brian.mangoenpawiro@amadeus.com>
2020-05-20 15:16:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Openshift Azure disconnected cleanup
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2017-09-01/network"
+
+	"github.com/Azure/go-autorest/autorest/azure/auth"
+	"github.com/Azure/go-autorest/autorest/to"
+)
+
